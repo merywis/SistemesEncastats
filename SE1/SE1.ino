4207193ff0edc7ef04ec78ad54e4e0ac2d0f9cf1
JaumeHimself <58419808+JaumeHimself@users.noreply.github.com>
2021-05-13 23:13:41 +0200

    Task KeyDetection ShareAdcValue InsertTemp guapita

--- SE1/SE1.ino
+++ SE1/SE1.ino
@@ -1,6 +1,7 @@
 //INCLUDEs
 #include "HIB.h"
 #include "SO.h"
+#include "timerConfig.h"
 HIB hib;
 SO so;
 
@@ -9,24 +10,41 @@ SO so;
 /** DEFINE ********************************************************************/
 /******************************************************************************/
 
-#define PRIO_TASK_A 2
+#define PRIO_TASK_KeyDetector 2
+#define PRIO_TASK_ShareAdcValue 3
+#define PRIO_TASK_InsertTemp 4
+
+
 
 /******************************************************************************/
 /** Global const and variables ************************************************/
 /******************************************************************************/
 
+volatile uint8_t key = 0;
 volatile uint16_t adcValue = 0;
-
-// flag (set of bits) for external events
-Flag fExtEvent;
-
-// This mask refers to the LSB of fExtEvent
-const unsigned char maskKeyEvent = 0x01; // represents new adc value adquired
 
 /********************************
   Declaration of flags and masks
 *********************************/
 
+// flag (set of bits) for external events
+Flag fKeyEvent;
+Flag fAdcEvent;
+
+// Masks
+const unsigned char maskKeyEvent = 0x01; // represents new adc value adquired
+const unsigned char maskAdcEvent = 0x01; // represents new adc value adquired 
+
+
+/*********
+  Declaration of semaphores
+*********/ 
+Sem sUART;
+
+/*********
+  Declaration of mailboxes
+*********/ 
+MBox mbNumRoom;
 
 
 /******************************************************************************/
@@ -40,6 +58,32 @@ const unsigned char maskKeyEvent = 0x01; // represents new adc value adquired
 /******************************************************************************/
 
 
+/*****************
+  KEYPAD hook
+******************/ 
+void keyHook(uint8_t newKey)
+{
+  
+  key = newKey;
+
+  so.setFlag(fKeyEvent, maskKeyEvent);  
+}
+
+
+/*****************
+  ADCISR
+******************/ 
+
+void adcHook(uint16_t newAdcAdquiredValue)
+{
+  adcValue = newAdcAdquiredValue;
+
+  hib.ledToggle(2); // for debugging
+
+  // Awake task A by setting to '1' the bits of fExtEvent indicated by maskAdcEvent
+  so.setFlag(fAdcEvent, maskAdcEvent);
+
+}
 
 /******************************************************************************/
 /** ISR *********************************************************************/
@@ -66,36 +110,62 @@ void State(int x){
 }
 
 
-void SimulateTempInt(int x){
+void InsertTemp(){
   
+  uint8_t NumRoom;
+  uint8_t * rxNumRoomMessage;
+  
+    while(true){ 
+      so.waitMBox(mbNumRoom,(byte**) &rxNumRoomMessage);
+      hib.ledToggle(1);  
+    }
 }
 
 
 
-void InsertComandos(int x)
-{
-  
-}
+void ShareAdcValue(){
 
-
-
-void KeyDetector(int x)
-{
   char str[16];
 
   while(1)
   {
     // Wait until any of the bits of the flag fExtEvent
     // indicated by the bits of maskAdcEvent are set to '1'        
-    so.waitFlag(fExtEvent, maskKeyEvent);
+    so.waitFlag(fAdcEvent, maskAdcEvent);
 
     // Print the new adc adquired value
     sprintf(str,"%u", adcValue);
     hib.lcdClear();
     hib.lcdPrint(str);
+
     
     // Clear the flag fExtEvent to not process the same event twice
-    so.clearFlag(fExtEvent, maskKeyEvent);
+    so.clearFlag(fAdcEvent, maskAdcEvent);
+  }  
+}
+
+
+
+void KeyDetector(){
+  
+  char str[16];
+
+  while(1)
+  {
+    // Wait until any of the bits of the flag fExtEvent
+    // indicated by the bits of maskAdcEvent are set to '1'        
+    so.waitFlag(fKeyEvent, maskKeyEvent);
+
+    // Print the new adc adquired value
+    sprintf(str,"%u", key);
+    hib.lcdClear();
+    hib.lcdPrint(str);
+    
+    if(key == 0 || key == 1 || key == 2 || key == 3){
+      so.signalMBox(mbNumRoom, (byte*) &adcValue);
+    }
+    // Clear the flag fExtEvent to not process the same event twice
+    so.clearFlag(fKeyEvent, maskKeyEvent);
   }  
 }
 
@@ -132,19 +202,32 @@ void loop() {
 
   // Rx vars
 
+
+  hib.keySetIntDriven(100, keyHook);
+  
+  //Set up adc
+  //hib.adcSetTimerDriven(TIMER_TICKS_FOR_125ms,(tClkPreFactType) TIMER_PSCALER_FOR_125ms, adcHook);
+  
   while (true){
 
     Serial.println(" ");
     Serial.println(" MAIN ");
 
     // Definition and initialization of flags
-    fExtEvent = so.defFlag();
+    fKeyEvent = so.defFlag();
+    fAdcEvent = so.defFlag();
    
      // Definition and initialization of tasks
-    so.defTask(KeyDetector, PRIO_TASK_A);
+    so.defTask(KeyDetector, PRIO_TASK_KeyDetector);
+    so.defTask(ShareAdcValue, PRIO_TASK_ShareAdcValue);
+    so.defTask(InsertTemp, PRIO_TASK_InsertTemp);
+
+    //
+    mbNumRoom = so.defMBox();
+    
   
     // Start mutltasking (program does not return to 'main' from hereon)
-   
+   so.enterMultiTaskingEnvironment(); // GO TO SCHEDULER
   }
   
 }
