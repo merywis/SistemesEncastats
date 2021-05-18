565f18d05cc0447651d6608b628729f274949a7d
Maria Orell Monserrat <mariaorell2000@gmail.com>
2021-05-18 15:42:57 +0200

    isrCan, share, state en SE1

--- SE1/SE1.ino
+++ SE1/SE1.ino
@@ -19,15 +19,21 @@ MCP_CAN CAN(SPI_CS_PIN);
 /******************************************************************************/
 /** DEFINE ********************************************************************/
 /******************************************************************************/
-
+#define NUM_ROOM 4
+#define PRIO_TASK_STATE 1
 #define PRIO_TASK_KeyDetector 2
 #define PRIO_TASK_ShareAdcValue 3
 #define PRIO_TASK_InsertTemp 4
 #define PRIO_TASK_InsertComandos 5
+#define PRIO_TASK_SHARE 6
 
 
 #define CAN_ID_TEMP_EXT 1
+#define CAN_ID_TEMP 2
+#define CAN_ID_TIME 3
 
+
+#define PERIOD_TASK_STATE 3
 #define PERIOD_TASK_INSERTCOMANDOS 2
 
 
@@ -36,15 +42,22 @@ MCP_CAN CAN(SPI_CS_PIN);
 /******************************************************************************/
 
 volatile uint8_t key = 0;
+volatile float rxTemp = 0.0; //tienen q ser globales ? :(
+volatile boolean rxTime = false; //tienen q ser globales ? :(
+
+volatile float TempExt = 0.0; //tienen q ser globales ? :(
+volatile boolean MomentDay = false;
 
 // stores every new adc adquired value
-// shared between: adhook (writes) and ShareAdcValue (reads)
+// shared between: adchook (writes) and ShareAdcValue (reads)
 volatile uint16_t adcValue = 0; // critical region beetween adcHook and ShareAdcValue
 
 // stores the last sampled value of the sensor
 // calculated upon adcValue
 // shared between ShareAdcValue (writes) and InsertTemp (reads)
 volatile float sampledAdc = 0.0; // critical region between ShareAdcValue and InsertTemp
+
+
 
 /********************************
   Declaration of flags and masks
@@ -58,11 +71,18 @@ Flag fAdcEvent;
 const unsigned char maskKeyEvent = 0x01; // represents new adc value adquired
 const unsigned char maskAdcEvent = 0x01; // represents new adc value adquired
 
+Flag fCANEvent;
+const unsigned char maskRxTempExtEvent = 0x01; // represents new adc value adquired
+const unsigned char maskRxTimeEvent = 0x02; // represents new adc value adquired
+
 
 /*********
   Declaration of semaphores
 *********/
 Sem sSampledAdc;
+Sem sTime;
+Sem sTempExt;
+Sem sGoal;
 
 /*********
   Declaration of mailboxes
@@ -74,13 +94,39 @@ MBox mbNumRoom;
 /** Definition of own types ***************************************************/
 /******************************************************************************/
 
+struct structRoom
+{
+  float tempGoal = 0.0; //
+  float tempInt = 0.0; //
+  uint8_t actuacion = 0;
+  float tempMaxDay = 0.0;
+  float tempMinDay = 0.0;
+  float tempMaxNight = 0.0;
+  float tempMinNight = 0.0;
+};
+typedef structRoom typeRoom;
+
+
+struct structState
+{
+  float tempExt = 0.0; // Number of edition
+  boolean momentDay = true; // true = day & false = night
+  typeRoom datosRoom1;
+  typeRoom datosRoom2;
+  typeRoom datosRoom3;
+  typeRoom datosRoom4;
+};
+typedef structState typeState;
+
+
 struct structGoal
 {
   uint8_t numRoom = 0; // Number of edition
   uint8_t tempGoal = 0; // Number of edition
 };
-
 typedef structGoal typeGoal;
+
+typeGoal Goal;
 
 struct structLimitesRoom {
   uint8_t maxDay = 0; // Number of edition
@@ -90,15 +136,15 @@ struct structLimitesRoom {
 };
 typedef structLimitesRoom typeLimitesRoom;
 
+
 struct structLimites
 {
-  structLimitesRoom limitesRoom1;
-  structLimitesRoom limitesRoom2;
-  structLimitesRoom limitesRoom3;
-  structLimitesRoom limitesRoom4;
+  typeLimitesRoom limitesRoom1;
+  typeLimitesRoom limitesRoom2;
+  typeLimitesRoom limitesRoom3;
+  typeLimitesRoom limitesRoom4;
 
 };
-
 typedef structLimites typeLimites;
 
 
@@ -144,10 +190,52 @@ void timer5Hook ()
   so.updateTime(); // Call SO to update time (tick) and manage tasks
 }
 
+
 /******************************************************************************/
 /** ISR *********************************************************************/
 /******************************************************************************/
 
+/*****************
+  CANISR
+******************/
+
+void isrCAN()
+{
+  const uint8_t RX_LED = 4;
+  char auxSREG;
+
+  // Save the AVR Status Register by software
+  // since the micro does not do it automatically
+  auxSREG = SREG;
+  ////////
+
+  if (CAN.rxInterrupt())
+  {
+    hib.ledToggle(RX_LED); // for debugging
+
+    CAN.readRxMsg();
+    switch (CAN.getRxMsgId())
+    {
+      case CAN_ID_TEMP:
+        CAN.getRxMsgData((byte*) &rxTemp);
+        so.setFlag(fCANEvent, maskRxTempExtEvent);
+        break;
+
+      case CAN_ID_TIME:
+        CAN.getRxMsgData((byte*) &rxTime);
+        so.setFlag(fCANEvent, maskRxTimeEvent);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  /////
+  // Restore the AVR Status Register by software
+  // since the micro does not do it automatically
+  SREG = auxSREG;
+}
 
 
 /******************************************
@@ -164,16 +252,109 @@ void timer5Hook ()
 
 */
 
-void State(int x) {
+void State()
+{
+  typeState state;
+  uint8_t numRoom = 0;
+  unsigned long nextActivationTick;
 
+  nextActivationTick = so.getTick();
+
+  while (true)
+  {
+
+    // Read adcValue (shared with ShareAdcValue)
+    so.waitSem(sTempExt);
+
+    state.tempExt = TempExt;
+
+    so.signalSem(sTempExt);
+
+    so.waitSem(sTime);
+
+    state.momentDay = MomentDay;
+
+    so.signalSem(sTime);
+
+    for (int i = 1; i < NUM_ROOM; i++) {
+
+      so.waitSem(sGoal);
+
+      numRoom = Goal.numRoom;
+
+      so.signalSem(sGoal);
+
+    if (numRoom == i){
+       state.datosRoomi.tempGoal = Goal.tempGoal;
+    }
+     
+    }
+
+
+
+
+
+
+    nextActivationTick = nextActivationTick +  PERIOD_TASK_STATE; // Calculate next activation time;
+    so.delayUntilTick(nextActivationTick);
+  }
 }
 
+
+void Share() {
+  // define a mask to awake either by maskAlarm or maskNotAlarm
+  unsigned char mask = (maskRxTempExtEvent | maskRxTimeEvent);
+  // var to read flag value
+  unsigned char flagValue;
+
+  while (true)
+  {
+    // Wait until any of the bits of the flag fSincro
+    // indicated by the bits of maskAlarm or maskNotAlarm are set to '1'
+    so.waitFlag(fCANEvent, mask);
+
+    // read flag Value
+    flagValue = so.readFlag(fCANEvent);
+
+    // Clear the mask bits of flag fSincro to not process the same event twice
+    so.clearFlag(fCANEvent, mask);
+
+    // distinguish whether the event has been alarm or not alarm
+    switch (flagValue)
+    {
+      case maskRxTempExtEvent:
+        Serial.println("maskRxTempExtEvent");
+
+        // Read adcValue (shared with ShareAdcValue)
+        so.waitSem(sTempExt);
+
+        TempExt = rxTemp;
+        hib.ledToggle(2);
+
+        so.signalSem(sTempExt);
+        break;
+
+      case maskRxTimeEvent:
+        Serial.println("maskRxTimeEvent");
+        so.waitSem(sTime);
+
+        MomentDay = rxTime;
+        hib.ledToggle(2);
+
+        so.signalSem(sTime);
+        break;
+
+      default:
+        break;
+    }
+
+  }
+}
 
 void InsertTemp() {
 
   uint8_t * rxNumRoomMessage;
 
-  typeGoal Goal;
   char str[16];
 
   while (true) {
@@ -295,13 +476,13 @@ void InsertComandos()
     // Check whether or not a byte has been received from UART
     if (c != term.NO_CHAR_RX_UART)
     {
-      arraylimitesRoom[i]=c;
+      arraylimitesRoom[i] = c;
       i++;
-      
+
       // Sent byte back to the PC
       term.print(c);
     }
-    if(i==4){
+    if (i == 4) {
       //Semaforo
       term.print( arraylimitesRoom[0]);
       term.print( arraylimitesRoom[1]);
@@ -363,6 +544,10 @@ void loop() {
 
     // Definition and initialization of semaphores
     sSampledAdc = so.defSem(1); // intially accesible
+    sTime = so.defSem(1); // intially accesible
+    sTempExt = so.defSem(1); // intially accesible
+    sGoal = so.defSem(1); // intially accesible
+
 
     // Definition and initialization of mailboxes
     mbNumRoom = so.defMBox();
@@ -370,12 +555,17 @@ void loop() {
     // Definition and initialization of flags
     fKeyEvent = so.defFlag();
     fAdcEvent = so.defFlag();
+    fCANEvent = so.defFlag();
 
     // Definition and initialization of tasks
+
+    so.defTask(State, PRIO_TASK_STATE);
     so.defTask(KeyDetector, PRIO_TASK_KeyDetector);
     so.defTask(ShareAdcValue, PRIO_TASK_ShareAdcValue);
     so.defTask(InsertTemp, PRIO_TASK_InsertTemp);
     so.defTask(InsertComandos, PRIO_TASK_InsertComandos);
+    so.defTask(Share, PRIO_TASK_SHARE);
+
 
     //Set up adc
     hib.adcSetTimerDriven(TIMER_TICKS_FOR_125ms, (tClkPreFactType) TIMER_PSCALER_FOR_125ms, adcHook);
